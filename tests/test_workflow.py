3d18f119dd6b67857797bdd86c396a9648ec26eb
Sara Taheri <srtaheri66@gmail.com>
2023-09-19 10:41:36 -0400

    added one example graph for testing conditional independencies

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -5,4 +5,21 @@ class TestWorkflow(unittest.TestCase):
 
     def test_fix_graph(self):
         # TODO: Add graphs and examples
-        pass
+        multi_med_confounder = NxMixedGraph.from_edges(
+            directed=[
+                (Z1, X),
+                (X, M1),
+                (M1, M2),
+                (M2, Y),
+                (Z1, Z2),
+                (Z2, Z3),
+                (Z3, Y),
+                (M1, R1),
+                (R1, R2),
+                (R2, R3),
+                (Y, R3),
+            ],
+            undirected=[
+                (Z1, X),
+            ],
+        )
