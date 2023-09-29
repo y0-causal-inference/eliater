9587a24f4df12503fb60ce7125c88a50ba7fa217
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-09-29 19:04:39 +0200

    Update

--- tests/test_discover_latent_nodes.py
+++ tests/test_discover_latent_nodes.py
@@ -7,7 +7,10 @@ from y0.dsl import Variable
 from y0.graph import set_latent
 
 from eliater.discover_latent_nodes import find_all_nodes_in_causal_paths, mark_latent
-from eliater.examples import multi_med, multi_med_confounder_nuisance_var
+from eliater.frontdoor_backdoor import (
+    multiple_mediators_single_confounder_example,
+    multi_mediators_confounders_nuisance_vars_example,
+)
 
 
 class TestDiscoverAndMarkLatentNodes(unittest.TestCase):
@@ -16,7 +19,9 @@ class TestDiscoverAndMarkLatentNodes(unittest.TestCase):
     def test_find_nodes_on_all_paths_for_multi_med(self):
         """Tests finding nodes in all causal paths for multi_med."""
         expected_nodes = {Variable("X"), Variable("M2"), Variable("M1"), Variable("Y")}
-        actual_nodes = find_all_nodes_in_causal_paths(multi_med, Variable("X"), Variable("Y"))
+        actual_nodes = find_all_nodes_in_causal_paths(
+            multiple_mediators_single_confounder_example.graph, Variable("X"), Variable("Y")
+        )
         self.assertEqual(expected_nodes, actual_nodes)
 
     def test_mark_latent_for_multi_med_confounder_nuisance_var(self):
@@ -25,9 +30,11 @@ class TestDiscoverAndMarkLatentNodes(unittest.TestCase):
         Test marking the descendants of nodes in all causal paths that are not ancestors of the outcome as latent
         nodes for multi_med_confounder_nuisance_var.
         """
-        expected_graph = deepcopy(multi_med_confounder_nuisance_var)
+        expected_graph = deepcopy(multi_mediators_confounders_nuisance_vars_example.graph)
         set_latent(expected_graph.directed, {Variable("R1"), Variable("R2"), Variable("R3")})
         actual_graph = mark_latent(
-            graph=multi_med_confounder_nuisance_var, treatments=Variable("X"), outcome=Variable("Y")
+            graph=multi_mediators_confounders_nuisance_vars_example.graph,
+            treatments=Variable("X"),
+            outcome=Variable("Y"),
         )
         self.assertEqual(expected_graph, actual_graph)
