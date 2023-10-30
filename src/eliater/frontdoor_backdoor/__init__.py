5838adcb4c6bfeede65d5f367367a5d61ef7d893
Sara Taheri <srtaheri66@gmail.com>
2023-10-30 12:53:27 -0400

    added more description for this module

--- src/eliater/frontdoor_backdoor/__init__.py
+++ src/eliater/frontdoor_backdoor/__init__.py
@@ -1,4 +1,23 @@
-"""This module contains multiple variations of the "frontdoor-backdoor" graph."""
+"""This module contains multiple variations of the "frontdoor-backdoor" graph.
+
+A frontdoor graph is a network structure where there is an exposure variable, and an
+outcome, and one or more variables on the directed path connecting exposure to the
+outcome. In addition, it contains one or more latent confounders between an exposure and the
+outcome. As the confounders are latent, the effect of exposure on the outcome can be estimated
+using Pearl's frontdoor criterion.
+
+A backdoor graph is a network structure where there is an exposure variable, and an
+outcome, and one or more observed confounders between an exposure and the
+outcome. As the confounders are observed, the effect of exposure on the outcome can be estimated
+using Pearl's backdoor criterion.
+
+A frontdoor-backdoor graph is designed to have the properties from both graph. It is a network that
+includes an exposure variable, and an outcome, and one or more variables on the directed path connecting
+exposure to the outcome. In addition, it contains one or more observed confounders between an exposure and the
+outcome. As the confounders are observed and mediators are present, the effect of exposure on the outcome can be
+estimated using Pearl's frontdoor or backdoor criterion.
+
+"""
 
 from y0.examples import Example
 
