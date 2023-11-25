902cc8d7a56847049b5b656be0c17d991ffeb79e
Sara Taheri <srtaheri66@gmail.com>
2023-11-25 12:28:19 -0500

    small change

--- src/eliater/examples/t_cell_signaling_pathway.py
+++ src/eliater/examples/t_cell_signaling_pathway.py
@@ -1,16 +1,25 @@
-"""Examples for T cell signaling pathway.
+"""Example for T cell signaling pathway.
 
-.. todo::
+This is an example of a protein signalling network of the T cell signaling pathway presented
+in [Sachs2005]_. It models the molecular mechanisms and regulatory processes of human cells involved
+in T cell activation, proliferation, and function.
 
-    1. wrap these with the :class:`y0.example.Example` class and detailed biological context.
-    2. Where did this network come from? Give reference
-    3. In what organism is this happening?
-    4. What is the biological phenomena described here?
-    5. How was this network constructed?
-    6. Is there associated data to go with this graph?
+Here is the data associated with this network:
+
+.. code-block:: python
+
+    data = pd.read_csv(
+    "../eliater/src/eliater/data/sachs_discretized_2bin.csv",
+    index_col=False,
+    )
+
+[Sachs2005] K. Sachs, O. Perez, D. Pe’er, D. A. Lauffenburger, and G. P. Nolan. Causal protein-signaling
+networks derived from multiparameter single-cell data. Science, 308(5721): 523–529, 2005.
 """
 
 from y0.graph import NxMixedGraph
+from y0.examples import Example
+from y0.algorithm.identify import Query
 
 graph = NxMixedGraph.from_str_adj(
     directed={
@@ -24,3 +33,16 @@ graph = NxMixedGraph.from_str_adj(
         "PIP2": ["PKC"],
     }
 )
+
+base_example = Example(
+    name="T cell signaling Example",
+    reference="K. Sachs, O. Perez, D. Pe’er, D. A. Lauffenburger, and G. P. Nolan. Causal protein-signaling"
+    "networks derived from multiparameter single-cell data. Science, 308(5721): 523–529, 2005.",
+    graph=graph,
+    description="This is an example of a protein signalling network of the T cell signaling pathway"
+    "It models the molecular mechanisms and regulatory processes of human cells involved"
+    "in T cell activation, proliferation, and function.",
+    example_queries=[Query.from_str(treatments="Raf", outcomes="Erk")],
+)
+
+base_example.__doc__ = base_example.description
