d44840c0baab47b5c70e1aeb581f19375b3624d2
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-12-04 18:18:04 +0100

    Add workflow for getting results for case studies
    cc @srtaheri

--- src/eliater/examples/__init__.py
+++ src/eliater/examples/__init__.py
@@ -8,8 +8,16 @@ from .sars import sars_cov_2_example
 from .t_cell_signaling_pathway import t_cell_signaling_example
 
 __all__ = [
+    "examples",
+    # actual examples
     "ecoli_transcription_example",
     "sars_cov_2_example",
     "t_cell_signaling_example",
     "single_mediator_with_multiple_confounders_nuisances_discrete_example",
+]
+
+examples = [
+    ecoli_transcription_example,
+    sars_cov_2_example,
+    t_cell_signaling_example,
 ]
