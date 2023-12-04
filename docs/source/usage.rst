acc7581a6b35c42548e1a5d9a5912625aac07f9a
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-12-04 13:08:13 +0100

    Add skeleton of high-level workflow (#15)
    This PR adds the `eliater.workflow` function, which combines the
    following:
    
    1. Add undirected edges between d-separated nodes for which a
    data-driven conditional independency test fails (waiting on #1, but
    there is a comment for later)
    2. Remove nuissance variables.
    3. Estimates the average causal effect (ACE) of the treatments on
    outcomes

--- docs/source/usage.rst
+++ docs/source/usage.rst
@@ -1,3 +1,9 @@
+Workflow
+========
+.. automodapi:: eliater.api
+    :no-heading:
+    :include-all-objects:
+
 Remove Nuisance Variables
 =========================
 .. automodapi:: eliater.discover_latent_nodes
--- src/eliater/__init__.py
+++ src/eliater/__init__.py
@@ -2,8 +2,10 @@
 
 """A high level, end-to-end causal inference workflow."""
 
+from .api import workflow
 from .discover_latent_nodes import remove_nuisance_variables
 
 __all__ = [
+    "workflow",
     "remove_nuisance_variables",
 ]
