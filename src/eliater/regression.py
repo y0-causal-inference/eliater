1390608f7290e94b422e0c60bc39d7764f97a551
Pruthvi Navada <pnavada.pruthvi@gmail.com>
2023-12-13 12:56:38 -0500

    Deriving adjustment sets

--- src/eliater/regression.py
+++ src/eliater/regression.py
@@ -20,10 +20,14 @@ is the direct effect X -> Y.
     with X will no longer represent the direct effect
 """
 
+from itertools import count
 from operator import attrgetter
 from typing import Sequence
 
 import pandas as pd
+from optimaladj.CausalGraph import CausalGraph
+from pgmpy.inference.CausalInference import CausalInference
+from pgmpy.models import BayesianNetwork
 from sklearn.linear_model import LinearRegression
 
 from y0.dsl import Variable
@@ -78,15 +82,54 @@ def fit_regression(
     return variables, model
 
 
+def to_causal_graph(graph: NxMixedGraph) -> CausalGraph:
+    """Convert y0 NxMixedGraph to equivalent optimaladj CausalGraph."""
+    causal_graph = CausalGraph()
+    ananke_admg = graph.to_admg()
+    causal_graph.add_edges_from(ananke_admg.di_edges)
+    counter = count(start=1)
+    for node1, node2 in ananke_admg.bi_edges:
+        latent = "U_{}".format(next(counter))
+        causal_graph.add_edge(latent, node1)
+        causal_graph.add_edge(latent, node2)
+    return causal_graph
+
+
+def to_bayesian_network(graph: NxMixedGraph) -> BayesianNetwork:
+    """Convert y0 NxMixedGraph to equivalent pgmpy BayesianNetwork."""
+    ananke_admg = graph.to_admg()
+    ananke_dag = ananke_admg.canonical_dag()
+    di_edges = ananke_dag.di_edges
+    bi_edges = ananke_admg.bi_edges
+    latents = ["U_{}_{}".format(*sorted([node1, node2])) for node1, node2 in bi_edges]
+    model = BayesianNetwork(ebunch=di_edges, latents=latents)
+    return model
+
+
 def get_optimal_adjustment_set(
     graph: NxMixedGraph, treatments: Variable | set[Variable], outcome: Variable
 ) -> set[Variable]:
     """Get the optimal adjustment set for estimating the direct effect of treatments on a given outcome."""
     treatments = _ensure_set(treatments)
-    raise NotImplementedError(
-        "see pruthvi's comment about copying ananke code: "
-        "https://olga-vitek-lab.slack.com/archives/C04NQ38BQ1Y/p1701985938163269"
-    )
+    if len(treatments) > 1:
+        raise NotImplementedError(
+            "Support for multiple treatments will be released in future versions."
+        )
+    treatments = list(treatments)
+    try:
+        causal_graph = to_causal_graph(graph)
+        non_latent_nodes = graph.to_admg().vertices
+        adjustment_set = causal_graph.optimal_minimum_adj_set(
+            treatment=treatments[0].name, outcome=outcome.name, L=[], N=non_latent_nodes
+        )
+    except Exception:
+        model = to_bayesian_network(graph)
+        inference = CausalInference(model)
+        adjustment_sets = inference.get_all_backdoor_adjustment_sets(
+            treatments[0].name, outcome.name
+        )
+        adjustment_set = min(adjustment_sets, key=len)
+    return adjustment_set
 
 
 def _demo():
