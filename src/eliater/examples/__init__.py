c63018729f6c98c608779377f876a05378d01855
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-12-14 10:19:53 +0100

    Create __init__.py

---
+++ src/eliater/examples/__init__.py
@@ -0,0 +1,19 @@
+"""Examples for eliater."""
+
+from .ecoli import ecoli_transcription_example
+from .sars import sars_cov_2_example
+from .t_cell_signaling_pathway import t_cell_signaling_example
+
+__all__ = [
+    "examples",
+    # actual examples
+    "ecoli_transcription_example",
+    "sars_cov_2_example",
+    "t_cell_signaling_example",
+]
+
+examples = [
+    ecoli_transcription_example,
+    sars_cov_2_example,
+    t_cell_signaling_example,
+]
