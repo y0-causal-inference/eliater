e714a575db323e0d959bfbf3bc6f4749de7a18c0
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-01-26 09:32:57 +0100

    Bump version: 0.0.1 → 0.0.2-dev

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f"{date.today().year}, Sara Taheri"
 author = "Sara Taheri"
 
 # The full version, including alpha/beta/rc tags.
-release = "0.0.1"
+release = "0.0.2-dev"
 
 # The short X.Y version.
 parsed_version = re.match(
